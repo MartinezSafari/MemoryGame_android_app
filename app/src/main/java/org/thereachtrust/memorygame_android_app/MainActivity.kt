2f3b9bd242bde1d479bf6638af993b7cd6d8dc95
martinez <martinez@thereachtrust.org>
2023-01-04 22:33:19 +0200

    layout tiles

--- app/src/main/java/org/thereachtrust/memorygame_android_app/MainActivity.kt
+++ app/src/main/java/org/thereachtrust/memorygame_android_app/MainActivity.kt
@@ -1,10 +1,30 @@
 package org.thereachtrust.memorygame_android_app
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity(), GameFragment.GameFragmentListener
+{
+
+
+   override fun makeTiles(): ArrayList<TextView>{
+        val tilesArray: ArrayList<TextView> = ArrayList()
+        for (i in 1..16){
+            val newTile= TextView(this)
+            newTile.text= "Hi"
+            newTile.setTextColor(Color.BLACK )
+            newTile.setBackgroundColor(Color.RED)
+
+            tilesArray.add(newTile)
+        }
+        return tilesArray
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
